7f273b2c209ccd7723fa6d6a04948248675d3e35
Erbby Danastian <erbby.danastian@iconpln.co.id>
2026-08-23 02:30:32 +0700

    Add ESP32 OTA firmware update support

--- firmware/PlatformIO/Projects/esp32_test/src/main.cpp
+++ firmware/PlatformIO/Projects/esp32_test/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
 
 #include "NetworkManager.h"
 #include "EncoderManager.h"
@@ -10,34 +11,82 @@ EncoderManager encoder;
 
 AudioManager audio;
 
+void setupOTA()
+{
+
+    ArduinoOTA.setHostname(
+        "amen-mixer");
+
+    ArduinoOTA.setPassword(
+        "amen123");
+
+    ArduinoOTA.onStart([]()
+                       {
+                           Serial.println(
+                               "OTA START");
+                       });
+
+    ArduinoOTA.onEnd([]()
+                     {
+                         Serial.println(
+                             "OTA COMPLETE");
+                     });
+
+    ArduinoOTA.onProgress(
+        [](unsigned int progress,
+           unsigned int total)
+        {
+            Serial.printf(
+                "OTA Progress: %u%%\n",
+                (progress * 100) / total);
+        });
+
+    ArduinoOTA.onError(
+        [](ota_error_t error)
+        {
+            Serial.printf(
+                "OTA ERROR: %u\n",
+                error);
+        });
+
+    ArduinoOTA.begin();
+
+    Serial.println(
+        "OTA READY");
+}
+
 void setup()
 {
 
-  Serial.begin(
-      115200);
+    Serial.begin(
+        115200);
 
-  delay(1000);
+    delay(1000);
 
-  Serial.println();
+    Serial.println();
 
-  Serial.println(
-      "=== AMEN START ===");
+    Serial.println(
+        "=== AMEN START ===");
 
-  network.begin();
+    network.begin();
 
-  encoder.begin();
+    encoder.begin();
 
-  audio.begin();
+    audio.begin();
 
-  Serial.println(
-      "BOOT");
+    setupOTA();
+
+    Serial.println(
+        "BOOT");
 }
 
 void loop()
 {
 
-  network.loop();
+    ArduinoOTA.handle();
 
-  encoder.update(
-      audio);
+    network.loop();
+
+    encoder.update(
+        audio);
 }
